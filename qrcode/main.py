0a536d7539271b8c8a0a536bff7439b7a8323cba
patricia <araujo.patricia.sa@gmail.com>
2025-06-06 11:31:39 -0300

    wip gera qrCode

--- qrcode/main.py
+++ qrcode/main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Projeto Python iniciado com sucesso!")
-    print("Estrutura de pastas criada corretamente.")
 
-if __name__ == "__main__":
-    main()
+import qrcode
+import customtkinter
+
+def qr():
+    entrada = entrada_usuario.get()
+    print(entrada)
+
+    imagem = qrcode.make()
+    imagem.save("qrcode2.png")
+
+    print("qrCode Gerado com Sucesso")
+
+
+janela = customtkinter.CTk()
+#Muda titulo
+janela.title("Gerador QrCode")
+
+#Muda tamanho da janela
+janela.geometry("300x400")
+
+#label para exebir na interface
+label_text = customtkinter.CTkLabel(janela, text="Digite o link do QRCODE")
+label_text.pack(padx=20, pady=20)
+
+#Entrada usuario
+entrada_usuario = customtkinter.CTkEntry(janela, placeholder_text="digite o link")
+entrada_usuario.pack(padx=20, pady=20)
+
+#botão
+buttom = customtkinter.CTkButton(janela, text="Gerar", command=qr())
+buttom.pack(padx=10, pady=20)
+
+janela.mainloop()
